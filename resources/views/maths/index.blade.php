abce596d44e3cac46edf81e6375e1c6ca0995a2f
kabedesu.co <ta_test@gmail.com>
2019-12-19 14:47:50 +0900

    math.index fixed

--- resources/views/maths/index.blade.php
+++ resources/views/maths/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.common')
 @section('content')
-    <div id="sansu" class="subject border-bottom border-info">
-        Math Page
+    <div id="calc_1" class="subject border-bottom border-info lead my-4 d-block text-sm-center">
+        tashizan
     </div>
+    <div id="calc_2" class="subject border-bottom border-info lead my-4 d-block text-sm-center">
+        hikizan
+    </div>
+    <div id="calc_3" class="subject border-bottom border-info lead my-4 d-block text-sm-center">
+        kuku
+    </div>
+    
 @endsection
