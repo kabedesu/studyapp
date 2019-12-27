5bb6d9bc7a30fcb643811d8f5ac94ab1a9a1c2bb
kabedesu.co <ta_test@gmail.com>
2019-12-27 14:40:19 +0900

    common.js added

--- public/js/common.js
+++ public/js/common.js
@@ -1,1 +1,13 @@
-alert('heyllo');
+document.addEventListener('DOMContentLoaded',() => {
+    const calc1_cont = document.getElementById('calc1_cont');
+    const test = $('#calc1_cont').textContent;
+    const calc1_btn = document.getElementById('calc1_btn');
+    const calc1_reset = document.getElementById('calc1_reset');
+    calc1_btn.addEventListener('click',()=> {
+        calc1_cont.textContent = "pushed";
+    });
+    calc1_reset.addEventListener('click',()=> {
+        calc1_cont.textContent = "1 + 1 = 2";
+    });
+    
+});
--- resources/views/maths/index.blade.php
+++ resources/views/maths/index.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.common')
 @section('content')
-    <div id="calc_1" class="subject border-bottom border-info">
-        tashizan
+    <div id="calc1" class="subject border-bottom border-info">
+        <div id="calc1_cont">1 + 1 = 2</div>
+        <div id="calc1_setting"></div>
+        <div class="btn-group btn-block" role="group" aria-label="keisan and reset">
+            <button id="calc1_btn" type="button" class="btn btn-info">keisan</button>
+            <button id="calc1_reset" type="button" class="btn btn-secondary">reset</button>
+        </div>
     </div>
-    <div id="calc_2" class="subject border-bottom border-info">
+    <div id="calc2" class="subject border-bottom border-info">
         hikizan
     </div>
-    <div id="calc_3" class="subject border-bottom border-info">
+    <div id="calc3" class="subject border-bottom border-info">
         kuku
     </div>
-    
+    <script type="text/javascript" src="{{ asset('/js/math.js') }}"></script>
 @endsection
